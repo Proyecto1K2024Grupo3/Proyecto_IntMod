6afe27fbdc977bbb43253d1cc8d50178ab625e76
Emilsito <emiliogarcalvo@gmail.com>
2025-01-20 23:59:12 +0100

    Agregadas 3 consultas agregadas mas
    Mostrar por cada empleado a cuántos clientes ha atendido
    Mostrar el precio total de los productos promocionados por cada proveedor junto al nombre de este
    Mostrar cuántas partidas se han jugado en cada sala

--- 712.sql
+++ 712.sql
@@ -14,9 +14,27 @@ group by j.cod_cliente;
 
 -- Mostrar por cada empleado a cuantos clientes ha atendido
 
+select e.cod_empleado, e.nombre as Empleado, count(a.cod_cliente) as ClientesAtendidos
+from EMPLEADO e
+    left join ATENDER a on e.cod_empleado = a.cod_empleado
+group by e.cod_empleado, e.nombre;
+
 -- Mostrar el precio total de los productos promocionados por cada proovedor junto al nombre de este
 
--- Mostrar cuantas partidas se hanjugado en cada sala
+select pr.nombre as Proveedor, sum(p.precio) as PrecioTotal
+from PROVEEDOR p
+    join PROVEER pv on pr.nif = pv.nif_Proveedor
+    join PRODUCTO p on pv.cod_producto = p.cod_producto
+    join PROMOCIONAR pro on p.cod_producto = pro.cod_producto
+group by pr.nombre;
+
+-- Mostrar cuantas partidas se han jugado en cada sala
+
+select s.n_sala as Sala, count(p.cod_partida) as PartidasJugadas
+from SALA s
+    left join PARTIDA p on s.n_sala = p.n_sala
+group by s.n_sala;
+
 
 
 
