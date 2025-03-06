c2c23f126800536cecfde0198aa2f1ab0d810a96
CaulShiverss <134002696+CaulShiverss@users.noreply.github.com>
2025-03-06 17:07:46 +0100

    Update EMPLEADO.java

--- JavaClases/EMPLEADO.java
+++ JavaClases/EMPLEADO.java
@@ -1,53 +1,101 @@
 package JavaClases;
 
 /**
- * 
+ * Clase que representa un empleado.
  */
-public class EMPLEADO {
+public class Empleado {
+    private int codEmpleado;
+    private String dni;
+    private int telefono;
+    private String fechaNacimiento;
+    private String nombre;
 
     /**
-     * Default constructor
+     * Constructor por defecto.
      */
-    public EMPLEADO() {
+    public Empleado(String dni, int codEmpleado, int telefono, String fechaNacimiento, String nombre) {
+        this.dni = dni;
+        this.codEmpleado = codEmpleado;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+    }
+
+    public int getCodEmpleado() {
+        return codEmpleado;
+    }
+
+    public void setCodEmpleado(int codEmpleado) {
+        this.codEmpleado = codEmpleado;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String toJson() {
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("{\n")
+                .append("    \"codEmpleado\": ").append(codEmpleado).append(",\n")
+                .append("    \"dni\": \"").append(dni).append("\",\n")
+                .append("    \"telefono\": ").append(telefono).append(",\n")
+                .append("    \"fechaNacimiento\": \"").append(fechaNacimiento).append("\",\n")
+                .append("    \"nombre\": \"").append(nombre).append("\"\n")
+                .append("}");
+        return jsonBuilder.toString();
+    }
+
+    public String toXML() {
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<Empleado>\n")
+                .append("    <codEmpleado>").append(codEmpleado).append("</codEmpleado>\n")
+                .append("    <dni>").append(dni).append("</dni>\n")
+                .append("    <telefono>").append(telefono).append("</telefono>\n")
+                .append("    <fechaNacimiento>").append(fechaNacimiento).append("</fechaNacimiento>\n")
+                .append("    <nombre>").append(nombre).append("</nombre>\n")
+                .append("</Empleado>");
+        return xmlBuilder.toString();
     }
 
     /**
-     * 
-     */
-    public int cod_empleado;
-
-    /**
-     * 
-     */
-    public String dni;
-
-    /**
-     * 
-     */
-    int telefono;
-
-    /**
-     * 
-     */
-    public String f_Nacimiento;
-
-    /**
-     * 
-     */
-    public String nombre;
-
-    /**
-     * 
+     * Método para registrar un empleado.
      */
     public void registrarEmpleado() {
         // TODO implement here
     }
 
     /**
-     * 
+     * Método para borrar un empleado.
      */
     public void borrarEmpleado() {
         // TODO implement here
     }
-
 }
