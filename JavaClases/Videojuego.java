8e01b42db9374ef2af331de115772fad16fc996c
pablo <pablomorillasesteve@gmail.com>
2025-03-06 17:11:34 +0100

    Videojuego toXML

---
+++ JavaClases/Videojuego.java
@@ -0,0 +1,52 @@
+package clases;
+
+/**
+ *
+ */
+public class Videojuego extends Producto {
+
+    private Consola consolaReemplazar;
+
+    // Propiedades
+
+    private String f_lanzamiento;
+
+    /**
+     * Default constructor
+     */
+
+    /**
+     * Default constructor
+     *
+     * @param nombre
+     * @param precio
+     * @param unidades
+     */
+
+        public Videojuego(String nombre, int precio, int unidades, String f_lanzamiento) {
+        super(nombre, precio, unidades);
+        this.f_lanzamiento = f_lanzamiento;
+    }
+
+    public String toJson() {
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("(\n")
+                .append("    \"precio\": ").append(getPrecio()).append(",\n")
+                .append("    \"unidades\": \"").append(getUnidades()).append("\",\n")
+                .append("    \"nombre\": ").append(getNombre()).append("\n")
+                .append("    \"f_lanzamiento\": ").append(f_lanzamiento).append("\n")
+                .append("}");
+        return jsonBuilder.toString();
+    }
+
+    public String toXML() {
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<Videojuego>\n")
+                .append("    <precio>").append(getPrecio()).append("</precio>\n")
+                .append("    <unidades>").append(getUnidades()).append("</unidades>\n")
+                .append("    <nombre>").append(getNombre()).append("</nombre>\n")
+                .append("    <f_lanzamiento>").append(f_lanzamiento).append("</nombre>\n")
+                .append("</Videojuego>");
+        return xmlBuilder.toString();
+    }
+}
