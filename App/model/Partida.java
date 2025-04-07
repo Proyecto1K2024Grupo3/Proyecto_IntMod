09432ab7266a4108b39b43f3a39ebf7e798a896e
“llorente” <serllogon2@alu.edu.gva.es>
2025-04-07 20:20:45 +0200

    Comentarios añadidos

--- App/model/Partida.java
+++ App/model/Partida.java
@@ -1,46 +1,61 @@
 package App.model;
 
 /**
- * Clase que representa una partida.
+ * @author: Pablo, Emilio y Sergio
  */
+
 public class Partida {
     private int codPartida;
     private int inicio;
     private int direccion;
 
     /**
-     * Constructor con parámetros.
+     * Constructor parametrizado de la clase Partida
+     * @param codPartida Codigo de la partida
+     * @param inicio Inicio de la partida
+     * @param fin Fin de la partida
+     * @param n_sala Numero de sala donde se juega la partida
      */
-    public Partida(int codPartida, int inicio, int direccion) {
+    public Partida(int codPartida, int inicio, int fin, int n_sala) {
         this.codPartida = codPartida;
         this.inicio = inicio;
         this.direccion = direccion;
     }
 
+    // Método getter para obtener el código de la partida
     public int getCodPartida() {
         return codPartida;
     }
 
+    // Método setter para establecer el código de la partida
     public void setCodPartida(int codPartida) {
         this.codPartida = codPartida;
     }
 
+    // Método getter para obtener el valor de inicio (puede representar el turno inicial, posición, etc.)
     public int getInicio() {
         return inicio;
     }
 
+    // Método setter para establecer el valor de inicio
     public void setInicio(int inicio) {
         this.inicio = inicio;
     }
 
+    // Método getter para obtener la dirección (podría representar la orientación, rumbo, o sentido en el juego)
     public int getDireccion() {
         return direccion;
     }
 
+    // Método setter para establecer la dirección
     public void setDireccion(int direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * Metodo que transforma la instancia de la clase a JSON
+     * @return Cadena JSON
+     */
     public String toJson() {
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.append("{\n")
@@ -51,6 +66,10 @@ public class Partida {
         return jsonBuilder.toString();
     }
 
+    /**
+     * Metodo que transforma la instancia de la clase a XML
+     * @return Cadena XML
+     */
     public String toXML() {
         StringBuilder xmlBuilder = new StringBuilder();
         xmlBuilder.append("<Partida>\n")
@@ -61,24 +80,4 @@ public class Partida {
         return xmlBuilder.toString();
     }
 
-    /**
-     * Método para crear una partida.
-     */
-    public void crearPartida() {
-        // TODO implement here
-    }
-
-    /**
-     * Método para borrar una partida.
-     */
-    public void borrarPartida() {
-        // TODO implement here
-    }
-
-    /**
-     * Método para actualizar una partida.
-     */
-    public void actualizarPartida() {
-        // TODO implement here
-    }
 }
